38bff23fa4d0f352a2f492f43ce9fe24291e0aa5
Goecha <goecha.ksh@gmail.com>
2024-05-19 23:59:35 +0530

    first commit

--- index.js
+++ index.js
@@ -39,8 +39,7 @@ app.use('/images', express.static('upload/images'))
 app.post("/upload", upload.single('product'), (req, res) => {
   res.json({
     success: 1,
-    // image_url: `http://localhost:${port}/images/${req.file.filename}`
-    image_url: `https://manipurmart-backend.onrender.com/images/${req.file.filename}`
+    image_url: `http://localhost:${port}/images/${req.file.filename}`
   })
 })
 
